8d711e7fb1420f74f1a75a49c911c9ce3dbba685
ChinLL <liloongchin@gmail.com>
2025-04-20 14:13:54 +0800

    Included String reply into Enquiry class

--- BTOManagementSystem/src/Enquiry.java
+++ BTOManagementSystem/src/Enquiry.java
@@ -5,6 +5,7 @@ public class Enquiry {
     private Applicant applicant;
     private BTOProject project;
     private String content;
+    private String reply;
     private LocalDateTime timestamp;
     private boolean isEdited;
     private LocalDateTime lastEditedTime;
@@ -14,6 +15,7 @@ public class Enquiry {
         this.applicant = applicant;
         this.project = project;
         this.content = content;
+        this.reply = null;
         this.timestamp = LocalDateTime.now();
         this.isEdited = false;
         this.lastEditedTime = null;
@@ -39,6 +41,14 @@ public class Enquiry {
         this.content = content;
         this.isEdited = true;
         this.lastEditedTime = LocalDateTime.now();
+    }
+
+    public void setReply(String reply) {
+        this.reply = reply;
+    }
+
+    public String getReply() {
+        return this.reply;
     }
 
     public LocalDateTime getTimestamp() {
