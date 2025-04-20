68931b992919d85c392269b4ac44dfa2b73fba9e
ChinLL <liloongchin@gmail.com>
2025-04-20 14:09:46 +0800

    Update: Modified BTO application and related manager classes, merged HDBOfficer with my implementation.

--- BTOManagementSystem/src/EnquiryManager.java
+++ BTOManagementSystem/src/EnquiryManager.java
@@ -3,12 +3,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class EnquiryManager {
+public class EnquiryManager implements IEnquiryManager {
     private static Map<Integer, Enquiry> enquiries = new HashMap<>();
     private static int nextEnquiryID = 1;
     
     // Submit a new enquiry
-    public static Enquiry submitEnquiry(Applicant applicant, BTOProject project, String content) {
+    @Override
+    public Enquiry submitEnquiry(Applicant applicant, BTOProject project, String content) {
         int enquiryID = nextEnquiryID++;
         Enquiry enquiry = new Enquiry(enquiryID, applicant, project, content);
         enquiries.put(enquiryID, enquiry);
@@ -16,7 +17,8 @@ public class EnquiryManager {
     }
     
     // Get all enquiries for an applicant
-    public static List<Enquiry> getEnquiriesForApplicant(Applicant applicant) {
+    @Override
+    public List<Enquiry> getEnquiriesForApplicant(Applicant applicant) {
         List<Enquiry> applicantEnquiries = new ArrayList<>();
         for (Enquiry enquiry : enquiries.values()) {
             if (enquiry.getApplicant().equals(applicant)) {
@@ -27,7 +29,8 @@ public class EnquiryManager {
     }
     
     // Get all enquiries for a project
-    public static List<Enquiry> getEnquiriesForProject(BTOProject project) {
+    @Override
+    public List<Enquiry> getEnquiriesForProject(BTOProject project) {
         List<Enquiry> projectEnquiries = new ArrayList<>();
         for (Enquiry enquiry : enquiries.values()) {
             if (enquiry.getProject().equals(project)) {
@@ -43,7 +46,8 @@ public class EnquiryManager {
     }
     
     // Update an enquiry
-    public static boolean updateEnquiry(int enquiryID, String newContent) {
+    @Override
+    public boolean updateEnquiry(int enquiryID, String newContent) {
         Enquiry enquiry = enquiries.get(enquiryID);
         if (enquiry == null) {
             return false;
@@ -54,7 +58,8 @@ public class EnquiryManager {
     }
     
     // Delete an enquiry
-    public static boolean deleteEnquiry(int enquiryID) {
+    @Override
+    public boolean deleteEnquiry(int enquiryID) {
         if (!enquiries.containsKey(enquiryID)) {
             return false;
         }
@@ -63,8 +68,32 @@ public class EnquiryManager {
         return true;
     }
     
+    // For applicants to get replies to their enquiries
+    @Override
+    public String getReply(int enquiryId) {
+        Enquiry enquiry = enquiries.get(enquiryId);
+        if (enquiry == null) {
+            return null;
+        }
+
+        return enquiry.getReply();
+    }
+    
     // Get all enquiries
     public static List<Enquiry> getAllEnquiries() {
         return new ArrayList<>(enquiries.values());
     }
+
+    // For officers to reply to enquiries
+    @Override
+    public boolean replyToEnquiry(int enquiryId, String message) {
+        Enquiry enquiry = enquiries.get(enquiryId);
+        if (enquiry == null) {
+            return false;
+        }
+
+        enquiry.setReply(message);
+        return true;
+    }
+
 } 
--- BTOManagementSystem/src/IEnquiryManager.java
+++ BTOManagementSystem/src/IEnquiryManager.java
@@ -1,5 +1,14 @@
+import java.util.List;
+
 public interface IEnquiryManager {
-    void submitEnquiry(Applicant applicant, String message);
-    void updateEnquiry(String enquiryId, String newMessage);
-    void handleEnquiry(String enquiryId); // applicants will use this to delete their enquiries while officers use it to respond
+    // For applicants
+    Enquiry submitEnquiry(Applicant applicant, BTOProject project, String content);
+    boolean updateEnquiry(int enquiryId, String newMessage);
+    boolean deleteEnquiry(int enquiryId); // APPLICANTS will use this to DELETE their enquiries
+    List<Enquiry> getEnquiriesForApplicant(Applicant applicant); // For APPLICANT to use
+    String getReply(int enquiryId); // For APPLICANTS to view replies to their enqueries
+
+    // For officers
+    boolean replyToEnquiry(int enquiryId, String message); // Officers will use this to REPLY to enquiries
+    List<Enquiry> getEnquiriesForProject(BTOProject project); // For OFFICER to use
 }
