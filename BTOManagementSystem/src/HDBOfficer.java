4a36182629ec6ab19d6a8aeb23e15b21a0f70f75
ChinLL <liloongchin@gmail.com>
2025-04-22 21:43:17 +0800

    HDBOfficer.java ..

--- BTOManagementSystem/src/HDBOfficer.java
+++ BTOManagementSystem/src/HDBOfficer.java
@@ -1,13 +1,18 @@
 public class HDBOfficer extends Applicant {
-    private BTOProject assignedProject;
-    private OfficerRegistrationManager officerRegistrationManager;
-    private EnquiryManager enquiryManager;
-    private BookingOpsManager bookingOpsManager;
+    private BTOProject assignedProject; // Officers can always access full project details, even when visibility is turned off.
     private RegistrationStatus registrationStatus; // whether registered to project
+    
+    public OfficerRegistrationManager officerRegistrationManager; // For registering for BTOProject
+    public EnquiryManager enquiryManager; 
+    public BookingOpsManager bookingOpsManager; // For managing flat booking requests by Applicants.
 
     public HDBOfficer(String name, String nric, int age, MaritalStatus maritalStatus, String password) {
         super(name, nric, age, maritalStatus, password);
         this.registrationStatus = RegistrationStatus.PENDING; // Default status
+        this.assignedProject = null;
+        this.officerRegistrationManager = new OfficerRegistrationManager();
+        this.enquiryManager = new EnquiryManager();
+        this.bookingOpsManager = new BookingOpsManager();
     }
 
     public void registerForProject(BTOProject project) {
@@ -20,17 +25,19 @@ public class HDBOfficer extends Applicant {
         }
     }
 
-
     public RegistrationStatus getRegistrationStatus() {
         return registrationStatus;
+    }
+
+    public BTOProject getAssignedProject() {
+        return this.assignedProject;
     }
 
     public void setRegistrationStatus(RegistrationStatus regStat) {
         this.registrationStatus = regStat;
     }
-    
 
     public void setAssignedProject(BTOProject project) {
-        
+        this.assignedProject = project;
     }
 }
